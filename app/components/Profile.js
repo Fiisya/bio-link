6a08adced1f1829583d2d9087157acd7dec38d26
ALFIXD <99767269+Fiisya@users.noreply.github.com>
2025-02-26 21:20:21 +0700

    work work

--- app/components/Profile.js
+++ app/components/Profile.js
@@ -30,7 +30,7 @@ const Profile = () => {
                 <p className="text-gray-400 text-xs">”Hanya Seorang Manusia Biasa Yang Ingin Menguasai Eropa:)”</p>
                 <div className="flex justify-center gap-2 mt-3">
                     <span className="bg-white/5 px-3 py-1 rounded-full text-xs hover:bg-white/10 transition-all hover:scale-105 ring-1 ring-green-700 text-yellow-400">
-                        #Coding
+                        #Work
                     </span>
                     <span className="bg-white/5 px-3 py-1 rounded-full text-xs hover:bg-white/10 transition-all hover:scale-105 ring-1 ring-green-700 text-yellow-400">
                         #Game
